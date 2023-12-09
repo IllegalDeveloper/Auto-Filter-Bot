67e2b2a84d727731d13bb47e802f0afb3b5adff2
Illegal Developer / <147461995+IllegalDevelopers@users.noreply.github.com>
2023-12-09 22:47:11 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,7 +9,7 @@ class script(object):
 ★ Language: Python
 ★ Library: Pyrogram</b>"""
 
-    SUPPORT_TXT = """<b>⍟ Cʜᴀɴɴᴇʟs & Gʀᴏᴜᴘs Mᴏᴅᴜʟᴇ ⍟
+    MY_SUPPORT_TXT = """<b>⍟ Cʜᴀɴɴᴇʟs & Gʀᴏᴜᴘs Mᴏᴅᴜʟᴇ ⍟
 🎬 Cᴏᴍᴘʟᴇᴛᴇ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛɪɴɢ Gʀᴏᴜᴘ.
 🚦 Aʟʟ Lᴀɴɢᴜᴀɢᴇs Mᴏᴠɪᴇs & Sᴇʀɪᴇs.
 🗣️ Bᴏᴛ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ.
